6bd233fff601cf7f6a7b12fdbbd3e987fbb67393
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-01-24 17:23:09 +0100

    Sequentially resolving promises for array of strings containing the text in the pages

--- src/components/PDFjs/PDFReader.js
+++ src/components/PDFjs/PDFReader.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import * as PdfJs from "pdfjs-dist";
 import { WORKER_URL } from "./WorkerLink";
+import { Promise as resolve } from "q";
 
 const Container = styled.div``;
 
@@ -11,7 +12,7 @@ class PDFReader extends Component {
     this.state = {};
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     // If absolute URL from the remote server is provided, configure the CORS
     // header on that server.
     const url = "dataset/paper1.pdf";
@@ -22,18 +23,45 @@ class PDFReader extends Component {
     const loadingTask = PdfJs.getDocument(url);
 
     loadingTask.promise.then(pdf => {
-      pdf.getPage(1).then(page => {
-        page.getTextContent().then(function(textContent) {
+      let pdfFullText = "";
+      const TOTAL_PAGES = pdf._pdfInfo.numPages;
+      const pagesPromises = [];
+
+      for (let p = 1; p <= TOTAL_PAGES; p++) {
+        pagesPromises.push(this.getPageText(p, pdf));
+      }
+
+      Promise.all(pagesPromises).then(function(pagesText) {
+        // Display text of all the pages in the console
+        console.log(pagesText);
+      });
+    });
+  }
+
+  /**
+   * Retrieves the text of a specif page within a PDF Document obtained through pdf.js
+   *
+   * @param {Integer} pageNum Specifies the number of the page
+   * @param {PDFDocument} PDFDocumentInstance The PDF document obtained
+   **/
+  getPageText(pageNum, PDFDocumentInstance) {
+    // Return a Promise that is solved once the text of the page is retrieven
+    return new Promise(function(resolve, reject) {
+      PDFDocumentInstance.getPage(pageNum).then(function(pdfPage) {
+        // The main trick to obtain the text of the PDF page, use the getTextContent method
+        pdfPage.getTextContent().then(function(textContent) {
           const textItems = textContent.items;
           let finalString = "";
 
           // Concatenate the string of the item to the final string
           for (let i = 0; i < textItems.length; i++) {
             const item = textItems[i];
+
             finalString += item.str + " ";
           }
 
-          console.log(finalString);
+          // Solve promise with the text retrieven from the page
+          resolve(finalString);
         });
       });
     });
