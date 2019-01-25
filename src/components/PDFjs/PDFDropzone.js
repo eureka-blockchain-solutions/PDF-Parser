7e105d5fce9f7b2c7d2018ea136266b0d986e374
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-01-25 10:39:16 +0100

    PDFDropzone with S3Upload

--- src/components/PDFjs/PDFDropzone.js
+++ src/components/PDFjs/PDFDropzone.js
@@ -3,6 +3,10 @@ import Dropzone from "react-dropzone";
 import styled from "styled-components";
 import { __FIFTH, __GRAY_300, __GRAY_500 } from "../../helpers/colors.js";
 import Icon from "../../views/icons/Icon.js";
+import S3Upload from "../S3Upload";
+import randomstring from "randomstring";
+import { getDomain } from "../../helpers/getDomain";
+import BarLoader from "../BarLoader";
 
 const DropZoneContainer = styled.div`
   display: flex;
@@ -12,6 +16,7 @@ const DropZoneContainer = styled.div`
   min-width: 200px;
   width: 60%;
   margin: 2em 0;
+  flex-direction: column;
 `;
 
 const StyledDropzone = styled(Dropzone)`
@@ -69,12 +74,72 @@ const DropperTitle = styled.h2`
   margin-top: 4px;
 `;
 
+const VerticalContainer = styled.div`
+  flex: 1;
+  flex-direction: column;
+  display: flex;
+`;
+
 class PDFDropzone extends React.Component {
-  onDrop = (acceptedFiles, rejectedFiles) => {
-    console.log(acceptedFiles);
-  };
+  constructor() {
+    super();
+    this.state = {
+      uploading: []
+    };
+  }
+
+  onDrop(accepted_files) {
+    const new_files = [];
+    accepted_files.forEach(file => {
+      const { name, type } = file;
+      const id = randomstring.generate(8);
+      const s3upload = new S3Upload({
+        s3_sign_put_url: `${getDomain()}/fileupload`,
+        onProgress: percent => {
+          this.setState({
+            uploading: this.state.uploading.map(u => {
+              if (u.id === id) {
+                return {
+                  id,
+                  progress: percent
+                };
+              }
+              return u;
+            })
+          });
+        },
+        onFinishS3Put: public_url => {
+          /*    this.props.onChangeFigure({
+            contents: [
+              {
+                url: public_url,
+                name,
+                type,
+                id: "f-" + Math.floor(Math.random() * 100000000)
+              }
+            ]
+          });*/
+          this.setState({
+            uploading: this.state.uploading.filter(u => u.id !== id)
+          });
+        },
+        onError: err => {
+          alert("Could not upload: " + err);
+        }
+      });
+      new_files.push({
+        id,
+        progress: 0
+      });
+      s3upload.uploadFile(file);
+    });
+    this.setState({
+      uploading: [...this.state.uploading, ...new_files]
+    });
+  }
 
   render() {
+    console.log(this.state.uploading);
     return (
       <DropZoneContainer>
         <StyledDropzone onDrop={this.onDrop.bind(this)}>
@@ -98,6 +163,13 @@ class PDFDropzone extends React.Component {
             );
           }}
         </StyledDropzone>
+        {this.state.uploading.map(u => {
+          return (
+            <VerticalContainer key={u.id}>
+              <BarLoader progress={u.progress} />
+            </VerticalContainer>
+          );
+        })}
       </DropZoneContainer>
     );
   }
