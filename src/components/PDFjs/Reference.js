47322c6c85a8ee3308ee29c9f1a8b409e56c5046
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-02-19 16:56:04 +0100

    entities and fields for guess row

--- src/components/PDFjs/Reference.js
+++ src/components/PDFjs/Reference.js
@@ -1,12 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import { __FIFTH, __GRAY_200 } from "../../helpers/colors";
+import { EXTRACT_ENTITIES } from "./EXTRACT_ENTITY_FACTORY";
+import GuessRow from "./GuessRow";
+import uuidv1 from "uuid";
 
 const Container = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
   border-bottom: 1px solid ${__GRAY_200};
   padding: 15px 10px 15px 0;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const Number = styled.div`
@@ -21,14 +29,47 @@ const Number = styled.div`
   text-align: center;
 `;
 
+const Body = styled.div``;
+
 const Text = styled.div`
-  margin-left: 10px;
+  margin-left: 12px;
 `;
+
+const getField = entity => {
+  return {
+    fName: entity.text,
+    lName: "",
+    id: uuidv1()
+  };
+};
 const Reference = ({ reference, ...otherProps }) => {
+  let entities = EXTRACT_ENTITIES(reference.reference);
   return (
     <Container>
-      <Number>{reference.number}</Number>
-      <Text>{reference.reference}</Text>
+      <Header>
+        <Number>{reference.number}</Number>
+        <Text>{reference.reference}</Text>
+      </Header>
+      <Body>
+        {entities.map(entity => {
+          const field = getField(entity);
+          return (
+            <GuessRow
+              field={field}
+              key={field.id}
+              confirmAuthor={id => {
+                //TODO: this.props.confirmAuthor(id);
+              }}
+              onDelete={id => {
+                // TODO: this.props.onDelete(id);
+              }}
+              onChange={(key, id, value) => {
+                // TODO: this.props.onChange(key, id, value);
+              }}
+            />
+          );
+        })}
+      </Body>
     </Container>
   );
 };
